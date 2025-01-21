989d9ce7a87be1d9a16e7067559ca9c96c20255e
Audun Kronstad <141585731+boolsen@users.noreply.github.com>
2025-01-21 23:00:27 +0100

    finished 04 js exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,21 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    for (arg of args) {
+        let finished = false;
+        let idx = 0;
+
+        do {
+            idx = array.indexOf(arg, idx);
+            if (idx >= 0) {
+                array.splice(idx,1);
+            }
+            length = array.length;
+        } while (idx >= 0 && length > 1);
+        
+    }
+    return array;
 };
+
+//removeFromArray([1, 2, 2, 3], 2)
 
 // Do not edit below this line
 module.exports = removeFromArray;
