6865c832ab5e39bd87162a63ae0f4d25da19a990
Audun Kronstad <141585731+boolsen@users.noreply.github.com>
2025-01-22 22:48:24 +0100

    finished 08 js practice

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;	
 };
 
-const sum = function() {
-	
+const sum = function(numArr) {
+  let total = 0;
+  for (num of numArr) {
+    total += num;
+  }	
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function(numArr) {
+  let product;
+  for (num of numArr) {
+    if (typeof product === 'undefined') {
+      product = num;
+    }
+    else {
+      product *= num;
+    }
+  }	
+  return product;
 };
 
-const power = function() {
-	
+const power = function(num1, num2) {
+  return num1 ** num2;	
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let total = 1;
+  for (let i = 1; i <= num; i++) {
+    total *= i;
+  }
+  return total;	
 };
 
 // Do not edit below this line
