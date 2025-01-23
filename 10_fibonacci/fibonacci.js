85f54781625ffc97a1e34eac0fce5fde9137e2a5
Audun Kronstad <141585731+boolsen@users.noreply.github.com>
2025-01-23 23:04:21 +0100

    finished 10 js practice

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let digit2 = 0;
+    let digit = 1;
+    let lastdigit = 0;
+    num = parseInt(num);
+
+    if (num === 0) {
+        return 0;
+    }
+    else if (num < 1) {
+        return "OOPS";
+    }
+
+    for (let i = 1; i < num; i++ ) {
+        lastdigit = digit;
+        digit += digit2;
+        digit2 = lastdigit;
+    }
+
+    return digit;
 
 };
+
+//fibonacci(6);
 
 // Do not edit below this line
 module.exports = fibonacci;
