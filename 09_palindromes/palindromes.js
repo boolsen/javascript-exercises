f125d3547b97cf8c2477d26ebcb1a290a9a5ffa0
Audun Kronstad <141585731+boolsen@users.noreply.github.com>
2025-01-23 22:49:57 +0100

    finished 09 js practice

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string.replace(/ /g,'').replace(' ','').replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g, '').toLowerCase();
+    let count = 0;
+    do {
+        if (string.charAt(count) != string.charAt(string.length - count - 1)) {
+            return false;
+        }
+        count++;
+    } while (count * 2 <= string.length);
+    return true;
 
 };
+
+//palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
