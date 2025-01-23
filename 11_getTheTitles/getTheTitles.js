bb8f6c23a50bd195940e40f8693b22d65325d3f3
Audun Kronstad <141585731+boolsen@users.noreply.github.com>
2025-01-23 23:17:39 +0100

    finished 11 js practice

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,12 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+    let titles = arr.map(findTitle);
+    return titles;
 
 };
+
+function findTitle(dict) {
+    return dict["title"];
+}
 
 // Do not edit below this line
 module.exports = getTheTitles;
